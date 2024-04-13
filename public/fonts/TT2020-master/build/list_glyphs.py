711b8fab7e05b130a6fb56506cb36648a99a58ef
jamibear <5701ja.mi@gmail.com>
2024-04-13 14:55:00 +0800

    delete font extras

--- public/fonts/TT2020-master/build/list_glyphs.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import sys
-sys.path.append("/usr/local/lib/python3.8/site-packages")
-import fontforge
-f = fontforge.open("../TT_ita.sfd")
-print('\n'.join(['{}'.format(c.glyphname) for c in f.glyphs("encoding")]))
--- public/fonts/TT2020-master/build/styleD.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ALT="$3" SEED=$((100000+$2+($4-1))) GIMPIMAGE="$1" python styleD.py
